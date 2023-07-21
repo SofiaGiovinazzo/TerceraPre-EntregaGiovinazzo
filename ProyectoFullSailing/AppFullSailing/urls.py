d027a3990fe4f1fcbc589a9ec97403dcc3970f70
Sofia Giovinazzo <sofanella@gmail.com>
2023-07-20 21:24:57 -0300

    DB Functional 3

--- ProyectoFullSailing/AppFullSailing/urls.py
+++ ProyectoFullSailing/AppFullSailing/urls.py
@@ -5,12 +5,11 @@ urlpatterns = [
     path('', views.inicio, name= 'inicio'),
     path('Alumnos', views.Alumnos, name= 'Alumnos'),
     path('Profesores', views.Profesores, name= 'Profesores'),
-    path('Examen', views.Examen, name= 'Examen'),
-    path('AlumnosFormulario', views.alumnosFormulario, name= 'AlumnosFormulario'),
+    path('Examen', views.Examenes, name= 'Examen'),
+    #path('AlumnosFormulario', views.alumnosFormulario, name= 'AlumnosFormulario'),
     path('ProfesoresFormulario', views.profesoresFormulario, name= 'ProfesoresFormulario'),
-    path('ExamenesFormulario', views.examenesFormulario, name= 'ExamenesFormulario'),
- 
- 
+    #path('ExamenesFormulario', views.examenesFormulario, name= 'ExamenesFormulario'),
+    path('busquedaEmail', views.controlFecha_examen, name='ControlFecha_examen'),
+    path('buscar/', views.buscar)
 
-    
 ]
--- ProyectoFullSailing/AppFullSailing/views.py
+++ ProyectoFullSailing/AppFullSailing/views.py
@@ -2,86 +2,91 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from AppFullSailing.models import Alumno, Profesor, Examen
 from AppFullSailing.forms import AlumnosFormulario, ProfesoresFormulario, ExamenesFormulario
+from django.http import HttpResponse
 
 
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio.html')
 
-def Alumnos(request):
+#def Alumnos(request):
     return render(request, 'Alumnos.html')
 
 def Profesores(request):
     return render (request, 'Profesores.html')
 
-def Examenes(request):
+#def Examenes(request):
     return render(request, 'Examen.html')
 
-def alumnosFormulario(request):
-    print("llegue1")
+def Alumnos(request):
     if request.method == 'POST': 
-        print("llegue2")
 
         miFormulario = AlumnosFormulario(request.POST) 
         print(miFormulario)
 
         if miFormulario.is_valid:
             informacion= miFormulario.cleaned_data
-            print("llegue3")
             
             alumnos = Alumno(nombre=informacion['nombre'], apellido=informacion['apellido'], edad=informacion['edad'], email=informacion['email'])
             alumnos.save()
             
             return render(request, 'inicio.html')
     else:
-        print('llegue4')
         miFormulario = AlumnosFormulario()
     
-    return render(request, 'AlumnosFormulario.html', {'miFormulario': miFormulario})
+    return render(request, 'Alumnos.html', {'miFormulario': miFormulario})
 
 def profesoresFormulario(request):
-    print("llegue1")
     if request.method == 'POST': 
-        print("llegue2")
 
         miFormulario = ProfesoresFormulario(request.POST) 
         print(miFormulario)
 
         if miFormulario.is_valid:
             informacion= miFormulario.cleaned_data
-            print("llegue3")
             
             profesores =Profesor(nombre=informacion['nombre'], apellido=informacion['apellido'], email=informacion['email'])
             profesores.save()
             
             return render(request, 'inicio.html')
     else:
-        print('llegue4')
         miFormulario = ProfesoresFormulario()
     
     return render(request, 'ProfesoresFormulario.html', {'miFormulario': miFormulario})
 
-def examenesFormulario(request):
-    print("llegue1")
+def Examenes(request):
     if request.method == 'POST': 
-        print("llegue2")
 
         miFormulario = ExamenesFormulario(request.POST) 
         print(miFormulario)
 
         if miFormulario.is_valid:
             informacion= miFormulario.cleaned_data
-            print("llegue3")
             
             examenes =Examen(embarcacion=informacion['embarcacion'], profe_examinador=informacion['profe_examinador'], fecha_examen=informacion['fecha_examen'])
             examenes.save()
             
             return render(request, 'inicio.html')
     else:
-        print('llegue4')
         miFormulario = ExamenesFormulario()
     
     return render(request, 'ExamenesFormulario.html', {'miFormulario': miFormulario})
 
+def controlFecha_examen(request):
+    return render(request,'controlFecha_examen.html' )
 
+def buscar(request):
+    if request.GET['fecha_examen']:
+
+        #respuesta = f'esta es la fecha buscada: {request.GET["fecha_examen"]}'
+        fecha_examen= request.GET['fecha_examen']
+        alumno= Examen.objects.filter(fecha_examen__icontains=fecha_examen)
+
+        return render(request, 'inicio.html', {'alumno': alumno, 'Fecha examen': fecha_examen})
+    
+    else:
+        respuesta = "No se encontraron los datos."
+
+    #return HttpResponse(respuesta)
+    return render(request, 'inicio.html', {'respuesta': respuesta})
 
